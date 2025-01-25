4b671f8f2cf52c26a315dc58128a59bdf40e2414
DamithBS <damithbuddika0420@gmail.com>
2025-01-25 14:53:58 +0530

    create a admin pages

--- src/pages/admin/adminPage.jsx
+++ src/pages/admin/adminPage.jsx
@@ -5,11 +5,11 @@ export default function AdminPage(){
     return(
         <div className="w-full h-screen flex">
         <div className="w-[400px] h-full bg-green-200">
-            <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><FaHome /> Dashboard</button>
-            <Link to="/admin/bookings" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Booking</Link>
-            <Link to="/admin/items" className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Items</Link>
-            <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' ><FaRegUser/> User</button>
-            <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Reviews</button>
+            <Link to={"/admin/"} className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><FaHome /> Dashboard</Link>
+            <Link to={"/admin/bookings"} className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Booking</Link>
+            <Link to={"/admin/items"} className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Items</Link>
+            <Link to={"admin/user"} className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' ><FaRegUser/> User</Link>
+            <Link to={"/admin/reviews"} className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center' >Reviews</Link>
         </div>
 
         <div className="w-[calc(100vw-400px)] bg-red-400">
