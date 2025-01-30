266a1a07086e7e67f129b5f6253a79defe7b1e10
DamithBS <damithbuddika0420@gmail.com>
2025-01-30 13:56:07 +0530

    create a login page . using hooks, 🔧

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AdminPage from './pages/admin/adminPage';
 import HomePage from './pages/home/homePage';
+import LoginPage from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster/>
       <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="/admin/*" element={<AdminPage />} />
         <Route path="/*" element={<HomePage />} />
       </Routes>
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import "./login.css";
+import { useState } from "react"
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginPage(){
+
+    const[email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+
+    const navigate = useNavigate();
+
+    function handleOnSubmit(e){
+        console.log(email,password);
+        e.preventDefault();
+
+        axios.post("http://localhost:3000/api/users/login",
+            {
+                email : email,
+                password : password
+            }
+        ).then((res)=>{
+            console.log(res);     
+            toast.success("Login Success.")
+            const user = res.data.user
+            if(user.role ==="admin"){
+                // window.location.href="/admin"
+                navigate("/admin/")
+            }
+            else{
+                // window.location.href="/"
+                navigate("/")
+            }
+
+        }).catch((err)=>{
+            console.log(err);
+            toast.error(err.response.data.error)
+        })
+        
+    }
+
+    return(
+        <div className="w-full h-screen flex justify-center items-center bg-picture">
+            <form onSubmit={handleOnSubmit}>
+                <div className="w-[400px] h-[400px] backdrop-blur-lg rounded-2xl flex justify-center items-center flex-col relative ">
+
+                    <img className="w-[100px] h-[100px] object-cover absolute top-2 rounded-full" src="/logo.jpg" alt="" />
+
+                    <input type="email" placeholder="Email" className="w-[300px] h-[30px] bg-transparent border-b-2 border-white text-white text-xl outline-none mt-9"
+                        value={email}
+                        onChange={
+                            (e)=>{
+                                setEmail(e.target.value)
+                                
+                            }
+                        }
+                    />
+
+                    <input type="password" placeholder="Password" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none" 
+                        value={password}
+                        onChange={
+                            (e)=>{
+                                setPassword(e.target.value)
+                            }
+                        }
+                    />
+
+                    <button className="my-8 w-[300px] h-[50px] bg-[#4543e2] text-2xl text-white rounded-lg">Login</button>
+
+                </div>
+            </form>
+        </div>
+
+    )
+
+}
