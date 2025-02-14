2403a23b530472c1eab2f4369cb361508acbfa27
DamithBS <damithbuddika0420@gmail.com>
2025-02-14 11:43:46 +0530

    create a register page and router it 📄

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AdminPage from './pages/admin/adminPage';
 import HomePage from './pages/home/homePage';
 import LoginPage from './pages/login/login';
 import { Toaster } from 'react-hot-toast';
+import RegisterPage from './pages/register/register';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
       <Routes>
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
         <Route path="/admin/*" element={<AdminPage />} />
         <Route path="/*" element={<HomePage />} />
       </Routes>
